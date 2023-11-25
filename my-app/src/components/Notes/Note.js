1336bf3c3c954c8f2fa58b9f7bb0b817ab72cb1b
Ofentse-T <122441914+Ofentse-T@users.noreply.github.com>
2023-11-25 12:15:16 +0200

    Edited the look of the page

--- my-app/src/components/Notes/Note.js
+++ my-app/src/components/Notes/Note.js
@@ -31,7 +31,7 @@ const Note = (props) => {
       onMouseOut={hoverOutHandler}
     >
       {isHover && (
-        <span className="material-icons check-circle">check_circle</span>
+        <span class="material-symbols-outlined check-circle">check_circle</span>
       )}
 
       <div className="title">{note.title}</div>
@@ -42,37 +42,37 @@ const Note = (props) => {
         style={{ visibility: isHover ? "visible" : "hidden" }}
       >
         <div className="tooltip">
-          <span className="material-icons-outlined hover small-icon">
+          <span className="material-symbols-outlined hover small-icon">
             add_alert
           </span>
           <span className="tooltip-text">Remind me</span>
         </div>
         <div className="tooltip">
-          <span className="material-icons-outlined hover small-icon">
+          <span className="material-symbols-outlined hover small-icon">
             person_add
           </span>
           <span className="tooltip-text">Collaborator</span>
         </div>
         <div className="tooltip">
-          <span className="material-icons-outlined hover small-icon">
+          <span className="material-symbols-outlined hover small-icon">
             palette
           </span>
           <span className="tooltip-text">Change Color</span>
         </div>
         <div className="tooltip">
-          <span className="material-icons-outlined hover small-icon">
+          <span className="material-symbols-outlined hover small-icon">
             image
           </span>
           <span className="tooltip-text">Add Image</span>
         </div>
         <div className="tooltip archive" onClick={deleteHandler}>
-          <span className="material-icons-outlined hover small-icon">
+          <span className="material-symbols-outlined hover small-icon">
             archive
           </span>
           <span className="tooltip-text">Archive</span>
         </div>
         <div className="tooltip">
-          <span className="material-icons-outlined hover small-icon">
+          <span className="material-symbols-outlined hover small-icon">
             more_vert
           </span>
           <span className="tooltip-text">More</span>
